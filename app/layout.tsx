027acea8c201511482189eb8ae9adbcfba757060
supastore <supastore1@outlook.com>
2023-01-18 18:34:13 -0500

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AnalyticsWrapper } from "./components/analytics"
 import "./globals.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -8,7 +9,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
 			<head />
-			<body>{children}</body>
+			<body>
+				{children}
+				<AnalyticsWrapper />
+			</body>
 		</html>
 	)
 }
