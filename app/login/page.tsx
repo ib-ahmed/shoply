4b7c29ce52e671aed54aef0bfa1c797f63f2166f
supastore <supastore1@outlook.com>
2023-01-22 18:02:18 -0500

    removed font override

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export default function Login() {
 	return (
 		<main className="flex flex-1 items-center justify-center">
 			<div className="flex w-full flex-col gap-4 rounded-md bg-zinc-900 p-2 sm:w-max sm:p-12">
-				<h2 style={heroSentenceFont.style} className="p-4 text-center text-xl font-bold dark:text-white">
+				<h2 style={heroSentenceFont.style} className="p-4 text-center text-xl dark:text-white">
 					Login
 				</h2>
 				<LoginForm />
